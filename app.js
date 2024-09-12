019d2d900d87d41b064a2ef7a7f67e7e4350199c
RosarioManny <rosario.emm47@gmail.com>
2024-09-12 14:32:27 -0400

    Unfinsihed

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const operator = document.querySelectorAll('.operator')
 const display = document.querySelector('.display')
 /*-------------------------------- Variables --------------------------------*/
 let calcDisplay; 
-let sbTract;
-let mltIply;
-let dVide; 
+let num1;
+let num2;
+let operation;
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -15,22 +15,36 @@ let updateDisplay = (value) => {
     display.innerText = value
 }
 
-
 calculator.addEventListener('click', (event) => {
-    console.log(event.target.innerText);
+    // console.dir(event.target.innerText);
     if (event.target.classList.contains('number')) {
+        if (!num1) {
+            num1 = parseInt(event.target.innerText)
+        } else if (!num2) {
+            num2 = parseInt(event.target.innerText)
+        }
         calcDisplay += event.target.innerText
-        console.log(calcDisplay)
+        updateDisplay(`${event.target.innerText}`)
     } else if (event.target.innerText === '+') {
-        addPlus()
+        operation = "add"
     } else if (event.target.innerText === '-'){
-
+        operation = "subtract"
     } else if (event.target.innerText === '/') {
-
+          operation = "divide"
     } else if (event.target.innerText === '*') {
-
+          operation = "multiply"
     } else if (event.target.innerText === "=") {
-
+        if (num1 & num2) {
+            if (operation === "add") {
+                addPlus(num1, num2)
+            } else if (operation === "subtract") {
+                subTract(num1, num2)
+            } else if (operation === "divide") {
+                diVide(num1, num2)
+            } else if (operation === "multiply") {
+                mltIply(num1, num2)
+            }
+        }
     } else if (event.target.innerText === "C") {
         clearBut()
     }
@@ -42,14 +56,30 @@ calculator.addEventListener('click', (event) => {
 //         console.log(calcDisplay)
 //     })
 // })
-const addPlus = () => {
-     return eval(`${calcDisplay} + ${calcDisplay}`)
+
+/*-------------------------------- Functions --------------------------------*/
+
+const addPlus = (num1, num2) => {
+    let sum = num1 + num2
+    updateDisplay(sum)
+}
+
+const subTract = (num1, num2) => {
+    let tra =  num1 - num2
+    updateDisplay(tra)
+    // console.log(tra)
+}
+
+const diVide = (num1, num2) =>  {
+    let vide = num1 / num2 
+    updateDisplay(vide)
+}
+
+const mltIply = (num1, num2) => {
+    let tiply = num1 * num2
+    updateDisplay(tiply)
 }
 const clearBut = () => {
     calcDisplay = ""
+    updateDisplay(calcDisplay)
 }
-/*-------------------------------- Functions --------------------------------*/
-
-// addPlus = () => {
-//   buttons.
-// }
